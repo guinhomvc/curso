da6c21b4ff6f8bb75b1d9e114bd814bb1a014da3
guinhomoraes <guilhermeguilherme98@gmail.com>
2018-06-17 11:40:27 -0300

    Adição das Views

---
+++ curso/app/Http/Controllers/Disciplina/DisciplinaController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Disciplina;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class \DisciplinaController extends Controller
+{
+    //
+}
---
+++ curso/app/Http/Controllers/Materia/MateriaController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Materia;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class \MateriaController extends Controller
+{
+    //
+}
